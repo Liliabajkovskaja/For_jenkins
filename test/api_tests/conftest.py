01485095b40a4d541df6144ad8f894f38e761738
mr_crow <merdenis91@gmail.com>
2024-04-23 20:25:58 +0200

    added jenkins lesson

--- test/api_tests/conftest.py
+++ test/api_tests/conftest.py
@@ -12,3 +12,19 @@ def test_create_user() -> UserDTO:
     payload = CreateUserPayload.random()
     response = user_api.create_user(body=payload.get_dict())
     return response
+
+#
+# # conftest.py
+# def get_data():
+#     data = []
+#     try:
+#         # request
+#         ...
+#     return data  # [{}, {}, ....]
+#
+#
+# # test.py
+# @pytest.mark.parametrize('data', get_data())
+# def test_requests_3(data):
+#     assert len(data) != 0, 'data is empty'
+#     # data ..
